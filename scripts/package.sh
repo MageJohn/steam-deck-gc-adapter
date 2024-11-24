f387f3039323dded1ed8c689a4540cc81ea783c5
Yuri Pieters <yuri@zopatista.com>
2024-11-24 17:01:43 +0000

    Update packaging to include the new files

--- scripts/package.sh
+++ scripts/package.sh
@@ -10,6 +10,7 @@ PROJECT_NAME=steam-deck-gc-adapter
 TAR_PATH="${ROOT}/${PROJECT_NAME}.tar.gz"
 PKG_DIR="${ROOT}/pkg"
 BIN_PATH="${ROOT}/wii-u-gc-adapter"
+TOOLS_PATH="${ROOT}/tools"
 
 if [[ ! -f "$BIN_PATH" ]]; then
   echo -e "${Red}wii-u-gc-adapter has not been built${RCol}"
@@ -21,5 +22,9 @@ rm -rf "$PKG_DIR"
 mkdir -p "${PKG_DIR}/${PROJECT_NAME}"
 
 cp -r etc "${BIN_PATH}" "${PKG_DIR}/${PROJECT_NAME}"
+
+mkdir -p "${PKG_DIR}/${PROJECT_NAME}/tools"
+cp "${TOOLS_PATH}/sdl_bind_append.py" "${TOOLS_PATH}/requirements.txt" \
+  "${PKG_DIR}/${PROJECT_NAME}/tools"
 
 tar -C "${PKG_DIR}" -caf "${TAR_PATH}" "${PROJECT_NAME}/"
