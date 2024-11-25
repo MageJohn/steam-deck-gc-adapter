f43e146a33f71778055a85845e7bf2501e298a3a
Yuri Pieters <yuri@zopatista.com>
2024-11-25 23:43:02 +0000

    Make paths in install.sh absolute

--- scripts/install.sh
+++ scripts/install.sh
@@ -6,9 +6,7 @@ ROOT=$(realpath "$SCRIPT_DIR/..")
 # shellcheck source=./lib/colours.sh
 source "$SCRIPT_DIR/lib/colours.sh"
 
-cd "$ROOT" || exit
-
-if [[ ! -x wii-u-gc-adapter ]]; then
+if [[ ! -x "$ROOT/wii-u-gc-adapter" ]]; then
   echo -e "${Red}wii-u-gc-adapter binary not found${RCol}"
   echo
   echo -e "This script should be run from the release archive,"
@@ -18,7 +16,8 @@ fi
 
 echo -e "${Blu}Installing udev rule (requires superuser)${RCol}"
 # pkexec works like sudo except no password needs to be set
-if ! pkexec install -Dvm 0664 etc/51-gcadapter.rules -t /etc/udev/rules.d/; then
+pkexec install -Dvm 0664 "$ROOT/etc/51-gcadapter.rules" -t /etc/udev/rules.d/
+if [[ $? == 126 || $? == 127 ]]; then
   echo -e "${Red}Could not acquire super user privileges. Aborting${RCol}"
   exit 1
 fi
@@ -28,12 +27,12 @@ if [[ -z "$XDG_CONFIG_HOME" ]]; then
 fi
 
 echo -e "${Blu}Installing systemd unit${RCol}"
-install -Dvm 0664 etc/wii-u-gc-adapter.service -t "$XDG_CONFIG_HOME/systemd/user"
+install -Dvm 0664 "$ROOT/etc/wii-u-gc-adapter.service" -t "$XDG_CONFIG_HOME/systemd/user"
 echo -e "${Blu}Installing wii-ugc-adapter executable${RCol}"
-install -Dvm 0755 wii-u-gc-adapter -t "$HOME/.local/bin"
+install -Dvm 0755 "$ROOT/wii-u-gc-adapter" -t "$HOME/.local/bin"
 
 echo -e "${Blu}Configuring GameCube controller bindings${RCol}"
-./scripts/sdl_bind_append.sh
+"$SCRIPT_DIR"/sdl_bind_append.sh
 
 echo -e "${Blu}Enabling systemd unit${RCol}"
 systemctl enable --user --now wii-u-gc-adapter.service || exit 1
