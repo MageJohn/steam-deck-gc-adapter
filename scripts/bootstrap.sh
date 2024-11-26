99a3e654eb5b3e3a97c919370e09964cf688c0d1
Yuri Pieters <yuri@zopatista.com>
2024-11-26 22:30:27 +0000

    Remove interactivity from bootstrap.sh
    The interactive part doesn't work when piping the script into bash (like
    `curl <url> | bash`). As this is the primary use case for the script,
    removing it is better.

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -16,24 +16,5 @@ tar -xaf steam-deck-gc-adapter.tar.gz || exit 1
 
 echo "Package downloaded and extracted."
 
-while true; do
-  # shellcheck disable=SC2162
-  read -p "Do you wish to inspect the files before installing? [y/N]: " yn
-  case $yn in
-    [Yy]*)
-      xdg-open steam-deck-gc-adapter 2> /dev/null
-      # shellcheck disable=SC2162
-      read -p "Hit enter to continue"
-      break
-    ;;
-    "") ;&
-    [Nn]*)
-      # just continue
-      break
-    ;;
-  esac
-done
-
-
 cd steam-deck-gc-adapter || exit 1
 exec ./scripts/install.sh
