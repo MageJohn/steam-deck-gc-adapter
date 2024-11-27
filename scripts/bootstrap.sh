f13dd16a06162838dfd07ac9ea84b4c2a7342795
Yuri Pieters <yuri@zopatista.com>
2024-11-27 01:06:01 +0000

    In bootstrap.sh use errexit

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
+set -o errexit
+
 on_exit(){
   echo "Something went wrong. Aborting installation"
 }
 
 trap on_exit EXIT
 
-cd /tmp || exit 1
+cd /tmp
 echo "Downloading package to /tmp"
 if ! curl -#LO https://github.com/MageJohn/steam-deck-gc-adapter/releases/download/v0.0.0/steam-deck-gc-adapter.tar.gz; then
   echo "Could not download package. Are you connected to the internet?"
   exit 1
 fi
-tar -xaf steam-deck-gc-adapter.tar.gz || exit 1
+tar -xaf steam-deck-gc-adapter.tar.gz
 
 echo "Package downloaded and extracted."
 
-cd steam-deck-gc-adapter || exit 1
+cd steam-deck-gc-adapter
 exec ./scripts/install.sh
